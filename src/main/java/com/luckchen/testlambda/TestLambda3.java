4598c2b0f033167517ffb24238d8ca0954cc78e8
chenyawei1227 <cyw119900@gmail.com>
2020-02-06 12:52:34 +0800

    新建分支 develop

--- src/main/java/com/luckchen/testlambda/TestLambda3.java
+++ src/main/java/com/luckchen/testlambda/TestLambda3.java
@@ -1,9 +1,16 @@
 package com.luckchen.testlambda;
 
+import org.junit.Test;
+
 /**
  * @Author: chenyawei
  * @Date: 2020-02-06  12:45
  * @Description:
  */
 public class TestLambda3 {
+
+    @Test
+    public void test1(){
+
+    }
 }
